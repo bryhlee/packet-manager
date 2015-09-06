bbe684b64cb38556aa26719cc7b2f64926c0a84d
bryhlee <bhlee@scu.edu>
2015-09-05 18:47:11 -1000

    Updated formatting and added more structure

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <iostream>
 
+const int MAX_SIZE = 1900;
+
 int main (int argc, char *argv[])
 {
 	std::cout << std::endl << "Packet Manager pkmg v 0.1" << std::endl;
@@ -18,23 +20,32 @@ int main (int argc, char *argv[])
 		std::cout << "> Error 255: Could not open file " << argv[1] << std::endl; 
 		return 255;
 	}
-	
-	std::cout << "Running diagnostics..." << std::endl;
-	
-	int i = 0;
-	char buffer[400];
-	while (!rwfile.eof())
+	else 
 	{
-		rwfile >> buffer[i++];
+		std::cout << "> File " << argv[1] << " opened successfully." << std::endl;
 	}
 	
-	std::cout << "Finished." << std::endl;
-	std::cout << "Number of characters in this packet: " << i << std::endl;
+	std::cout << "> Running diagnostics..." << std::endl;
 	
+	int i = 0;
+	char buffer[MAX_SIZE];
+	int buffersize = 0;
 	int checksum = 0;
-	for (i = 0; i < 400; i++)
+	
+	while (!rwfile.eof())
 	{
-		checksum+=buffer[i++];
+		rwfile.get(buffer[i++]);
+		buffersize++;
+	}
+	buffersize-1;
+	
+	std::cout << "> Finished." << std::endl;
+	std::cout << "Number of characters in this packet: " << buffersize << std::endl;
+	
+	
+	for (i = 0; i < buffersize; i++)
+	{
+		checksum += (int)buffer[i];
 	}
 	std::cout << "Checksum: " << checksum << std::endl;
 	
